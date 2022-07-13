c12f14a7f0df441ea27ae676d9bdaf3b0365a407
___hanzel___ <77740697+Sabotron@users.noreply.github.com>
2022-07-13 01:41:03 -0600

    Add files via upload

---
+++ src/src/pages/NoPage.js
@@ -0,0 +1,11 @@
+const NoPage = () => {
+
+  return (
+    <div className='Home'>
+                  <h1>Error 404</h1>
+        <h3>Page not found!</h3>
+        </div>
+  )
+  }  
+  export default NoPage;
+  
